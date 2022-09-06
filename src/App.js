70c5e0e82db2f26d24f4da0417257eb0b24991ae
Nineteen991 <Nineteen99@gmail.com>
2022-09-05 22:34:39 -0700

    quick and easy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
+import { useState } from 'react'
+
+import nachos from './nachos.jpg'
 import './App.sass';
 
 export default function App() {
+  const [openModal, setOpenModal] = useState(false)
+
+  const popup = 
+    <div className='banner'>
+      <img 
+        src={nachos} 
+        alt='nachos' 
+        id='banner-nachos'
+      />
+      <h1>Yes please!</h1>
+    </div>
+  
   return (
-    <div>hi</div>
+    <div className='container'>
+      {
+        openModal
+          ? <>
+              <div className='shadow' onClick={() => setOpenModal(false)}></div>
+              { popup }
+            </>
+          : <img 
+              src={nachos} 
+              alt='nachos' 
+              id='nachos' 
+              onClick={() => setOpenModal(true)} 
+            />
+      }
+    </div>
   )
 }
